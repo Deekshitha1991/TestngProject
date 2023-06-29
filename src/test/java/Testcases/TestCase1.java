bfc8009c6aff32f61b3913380256262063ffcc2b
deekshitha <mudigantideekshitha@gmail.com>
2023-06-29 18:38:19 -0500

    adding new file

---
+++ src/test/java/Testcases/TestCase1.java
@@ -0,0 +1,13 @@
+package Testcases;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@Test	
+	public void doLogin()
+	{
+		System.out.println("executing the method");
+	}
+
+}
